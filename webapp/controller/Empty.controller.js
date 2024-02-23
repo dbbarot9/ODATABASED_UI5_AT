79d1747873c9af69d946a9c9ed060fbb7e7833e7
dbbarot9 <dhavalbarot.in@gmail.com>
2024-02-23 15:01:08 -0800

    third  commit at 3 pm 23-Feb

--- webapp/controller/Empty.controller.js
+++ webapp/controller/Empty.controller.js
@@ -1,7 +1,5 @@
 sap.ui.define(['sapOdata/controller/BaseController'],
  function(BaseController) {
-    'use strict';
-    return BaseController.extend('sapOdata.controller.Empty',{
-
+    'use 
     });
 });
